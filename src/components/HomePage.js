1612b31219b239f0f87f26623e32ef2cdb3d8c7c
Ali <134099889+b70b70@users.noreply.github.com>
2023-11-06 23:52:39 +0000

    added submit button for reg input

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { AppstoreOutlined, SearchOutlined, SettingOutlined } from '@ant-design/icons';
-import { Menu } from 'antd';
+import { Menu, Button } from 'antd';
 import '../index.css';
 
 const items = [
@@ -60,35 +60,40 @@ const items = [
 ];
 
 const HomePage = () => {
-    const [current, setCurrent] = useState('mail');
-    const onClick = (e) => {
-      console.log('click ', e);
-      setCurrent(e.key);
-    };
-  
-    return (
-      <div>
-        {/* Inner Header with Logo */}
-        <div className="inner-header">
-          <img src="/your-logo.png" alt="Logo" />
-        </div>
-  
-        {/* Navigation Bar */}
-        <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
-          {items.map(item => (
-            <Menu.Item key={item.key} icon={item.icon} disabled={item.disabled}>
-              {item.label}
-            </Menu.Item>
-          ))}
-        </Menu>
-  
-        {/* Centered Banner with <h1> and <p> */}
+  const [current, setCurrent] = useState('mail');
+  const onClick = (e) => {
+    console.log('click ', e);
+    setCurrent(e.key);
+  };
+
+  return (
+    <div>
+      {/* Inner Header with Logo */}
+      <div className="inner-header">
+        <img src="/your-logo.png" alt="Logo" />
+      </div>
+
+      {/* Navigation Bar */}
+      <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal">
+        {items.map(item => (
+          <Menu.Item key={item.key} icon={item.icon} disabled={item.disabled}>
+            {item.label}
+          </Menu.Item>
+        ))}
+      </Menu>
+
+      {/* Centered Banner with <h1> and <p> */}
       <div className="banner">
         <div className="banner-content">
           <h1>Enter your Reg to find your Auto Repair</h1>
           <p>Some additional text goes here.</p>
           <input type="text" className="regInput" placeholder="ENTER REG" />
         </div>
+      </div>
+
+      {/* Button Container with Ant Design Button */}
+      <div className="searchButton-container">
+        <Button type="primary" icon={<SearchOutlined />}>Search</Button>
       </div>
     </div>
   );
