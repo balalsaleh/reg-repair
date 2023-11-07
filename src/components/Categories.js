b1a378e3d9ccfc3570a2ebc29970b439047f95b6
Ali <134099889+b70b70@users.noreply.github.com>
2023-11-07 21:23:12 +0000

    improved UI for car part categories & added icons

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import { Card } from 'antd';
-const gridStyle = {
-  width: '25%',
-  textAlign: 'center',
-};
+import "../index.css";
+import brakesIcon from "./images/brake-icon.svg";
+import batteryIcon from "./images/battery-icon.svg";
+import bulbIcon from "./images/lamp-icon.svg";
+import oilIcon from "./images/oil-icon.svg";
+import wiperIcon from "./images/wiper-icon.svg";
+
 const App = () => (
-  <Card title="Card Title">
-    <Card.Grid style={gridStyle}>Content</Card.Grid>
-    <Card.Grid hoverable={false} style={gridStyle}>
-      Content
-    </Card.Grid>
-    <Card.Grid style={gridStyle}>Content</Card.Grid>
-    <Card.Grid style={gridStyle}>Content</Card.Grid>
-    <Card.Grid style={gridStyle}>Content</Card.Grid>
-    <Card.Grid style={gridStyle}>Content</Card.Grid>
-    <Card.Grid style={gridStyle}>Content</Card.Grid>
+  <Card title="Car Part" className="grid-container">
+    <div className="grid-row">
+      <div className="grid-item">
+        <img src={brakesIcon} alt="Brakes Icon" />
+        Brakes
+      </div>
+      <div className="grid-item">
+        <img src={wiperIcon} alt="Wipers Icon" />
+        Wipers
+      </div>
+      <div className="grid-item">
+        <img src={batteryIcon} alt="Battery Icon" />
+        Battery
+      </div>
+      <div className="grid-item">
+        <img src={bulbIcon} alt="Bulbs Icon" />
+        Bulbs
+      </div>
+      <div className="grid-item">
+        <img src={oilIcon} alt="Oil Icon" />
+        Engine Oil
+      </div>
+    </div>
   </Card>
 );
+
 export default App;
