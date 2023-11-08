682d463225eb622332b4e36c0c538da532182e44
Balal Saleh <balal_saleh@outlook.com>
2023-11-08 18:19:12 +0000

    changed categories to have buttons

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Card } from 'antd';
+import React from "react";
+import { Card } from "antd";
 import "../index.css";
 import brakesIcon from "./images/brake-icon.svg";
 import batteryIcon from "./images/battery-icon.svg";
@@ -10,26 +10,26 @@ import wiperIcon from "./images/wiper-icon.svg";
 const App = () => (
   <Card title="Car Part" className="grid-container">
     <div className="grid-row">
-      <div className="grid-item">
+      <button className="grid-item">
         <img src={brakesIcon} alt="Brakes Icon" />
         Brakes
-      </div>
-      <div className="grid-item">
+      </button>
+      <button className="grid-item">
         <img src={wiperIcon} alt="Wipers Icon" />
         Wipers
-      </div>
-      <div className="grid-item">
+      </button>
+      <button className="grid-item">
         <img src={batteryIcon} alt="Battery Icon" />
         Battery
-      </div>
-      <div className="grid-item">
+      </button>
+      <button className="grid-item">
         <img src={bulbIcon} alt="Bulbs Icon" />
         Bulbs
-      </div>
-      <div className="grid-item">
+      </button>
+      <button className="grid-item">
         <img src={oilIcon} alt="Oil Icon" />
         Engine Oil
-      </div>
+      </button>
     </div>
   </Card>
 );
